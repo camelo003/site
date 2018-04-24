68d6311c334db3c7ad640cd2250368ed7b4d153c
camelo003 <holysmoke003@gmail.com>
2018-04-24 01:58:12 -0300

    Add files via upload

--- docs/totemnation/js/index.js
+++ docs/totemnation/js/index.js
@@ -5,68 +5,104 @@ TO-DOS:
 3. [  ] Calcular loop dinamicamente;
 4. [  ] Pause quando enquanto 'over';
 5. [  ] Ordem randômica;
-6. [  ] Puxar gifs.
+6. [ok] Puxar gifs.
+7. [ok] Loading (https://github.com/processing/p5.js/wiki/p5.js-overview#loading-screen)
+8. [  ] reestruturar o totem:
+        -uma div para o totem
+          -uma div para o andar
+            -o gif
+            -o link
+
 */
 
-var container;
+var comtainer;
 var totem;
+var isOverTotem = false;
 
 var entries = [
-  {key: "div1", credit: ".camelo", link: "https://twitter.com/camelo003", div: ""},
-  {key: "div2", credit: ".camelo", link: "https://twitter.com/camelo003", div: ""},
-  {key: "div3", credit: ".camelo", link: "https://twitter.com/camelo003", div: ""},
-  {key: "div4", credit: ".camelo", link: "https://twitter.com/camelo003", div: ""},
-  {key: "div5", credit: ".camelo", link: "https://twitter.com/camelo003", div: ""},
-  {key: "div6", credit: ".camelo", link: "https://twitter.com/camelo003", div: ""},
-  {key: "div7", credit: ".camelo", link: "https://twitter.com/camelo003", div: ""}
+  {key: "l1IBhYOZfhLz2NfSU", credit: "Pedro Miranda Filho", link: "https://giphy.com/pedromirfilho/"},
+  {key: "3o751YFJcflc0SOBS8", credit: "Pedro Miranda Filho", link: "https://giphy.com/pedromirfilho/"},
+  {key: "1yMfhX748Xzo5fF2d4", credit: "Pedro Miranda Filho", link: "https://giphy.com/pedromirfilho/"},
+  {key: "26gN0Jn614shuSf2U", credit: "Pedro Miranda Filho", link: "https://giphy.com/pedromirfilho/"},
+  {key: "3o752gFScyte7n6fCM", credit: "Pedro Miranda Filho", link: "https://giphy.com/pedromirfilho/"},
+  {key: "l0HUeDZfTRQuKIvCg", credit: "Pedro Miranda Filho", link: "https://giphy.com/pedromirfilho/"},
+  {key: "1lvTGOdg47rhHP0d61", credit: "Pedro Miranda Filho", link: "https://giphy.com/pedromirfilho/"}
 ];
-var complete = {};
 
 var vel = 0.5;
 
-function initDiv(entrie){
-  entrie.div = createDiv(entrie.key);
-  entrie.div.class("totemFloor");
-  entrie.div.parent(totem);
-  var a = createA(entrie.link,entrie.credit);
-  a.parent(totem);
+function initAndFillDiv(entrie){
+  var floor  = createDiv("");
+  floor.class("totemFloor");
+  floor.parent(totem);
+  var img = createImg(entrie.json.data.images.original.url);
+  img.parent(floor);
+  img.style("width","400px");
+  img.style("height","180px");
+  var link = createA(entrie.link,entrie.credit);
+  link.class("link");
+  link.parent(floor);
 }
 
 function fillComplete(amount){
   for(var i=0;i<amount;i=i+1){
-    complete[i] = {key: entries[i].key,credit: entries[i].credit,link: entries[i].link,div: entries[i].div};
-    initDiv(complete[i]);
+    initAndFillDiv(entries[i]);
   }
 }
 
-var json = {};
+function overTotem(){
+  //vel = 0.0;
+  isOverTotem = true;
+}
+
+function outTotem(){
+  //vel = 0.5;
+  isOverTotem = false;
+}
+
+function mouseClicked(){
+  print(winMouseX + " " + winMouseY);
+}
+
+function overUpdate(status){
+  if(status){
+    vel = vel - 0.02;
+    if(vel < 0){
+      vel = 0.0;
+    }
+  }else{
+    vel = vel + 0.02;
+    if(vel > 0.5){
+      vel = 0.5;
+    }
+  }
+}
 
 function preload(){
-  var url = "http://api.giphy.com/v1/gifs/" + "26gN0Jn614shuSf2U" + "?api_key=dc6zaTOxFJmzC";
-  json = loadJSON(url);
+  for(var i=0;i<entries.length;i=i+1){
+    var url = "http://api.giphy.com/v1/gifs/" + entries[i].key + "?api_key=dc6zaTOxFJmzC";
+    entries[i].json = loadJSON(url);
+  }
 }
 
 function setup(){
-  { //puxa imagem e coloca no html!
-    var tempLink = json.data.images.original.url
-    createImg("http" + tempLink.substring(5));
-    print("http" + tempLink.substring(5));
-  }
+  noCanvas();
 
-  container = createDiv("");
-  container.class("cont");
+  comtainer = select("#container");
+  comtainer.class("cont");
 
   totem = createDiv("");
   totem.class("totem");
-  totem.parent(container);
+  totem.parent(comtainer);
 
   for(var i=0;i<entries.length;i=i+1){
-    initDiv(entries[i]);
+    initAndFillDiv(entries[i]);
   }
 
-  fillComplete(6);
+  fillComplete(Math.ceil(500/180));
 
-  noCanvas();
+  totem.mouseOver(overTotem);
+  totem.mouseOut(outTotem);
 }
 
 function draw(){
@@ -80,9 +116,11 @@ function draw(){
   } //read, increment and update css translation!
 
   {
-    //(100 for totem floor heught and 7 for entries.length)
-    if(e < -(100*7)+1){
+    //(180 for totem floor height)
+    if(e < -(180*entries.length)+1){
        totem.style("transform", "matrix(1, 0, 0, 1, 0, 0)");
        }
   } //check and loop translation!
+
+  overUpdate(isOverTotem);
 }
